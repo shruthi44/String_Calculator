0a8086daa3ce29ea8a12e2fae98fe7b26fe0d6ba
Vanessa Wingårdh <vwingardh@gmail.com>
2023-05-15 14:23:45 +0200

    Created tests for add() input

--- src/test/java/calculator/StringCalculatorTest.java
+++ src/test/java/calculator/StringCalculatorTest.java
@@ -18,4 +18,27 @@ class StringCalculatorTest {
     public void addShouldReturnZeroEmptyString() {
         assertEquals(0, calculator.add(new String[]{""}));
     }
+
+    @Test
+    public void addShouldThrowExceptionInvalidInputCharacter() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> calculator.add(new String[]{"4, h"}));
+        assertEquals("Invalid input", ex.getMessage());
+    }
+
+    @Test
+    public void addShouldThrowExceptionInvalidInputSpace() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> calculator.add(new String[]{"4, 5 6"}));
+        assertEquals("Invalid input", ex.getMessage());
+    }
+
+    @Test
+    public void addShouldThrowExceptionInvalidInputSpecialCharacter() {
+        IllegalArgumentException ex = assertThrows(IllegalArgumentException.class, () -> calculator.add(new String[]{"4, 5 !"}));
+        assertEquals("Invalid input", ex.getMessage());
+    }
+
+    @Test
+    public void addShouldReturnSumOfIntegers() {
+        assertEquals(9, calculator.add(new String[]{"4, 5"}));
+    }
 }
