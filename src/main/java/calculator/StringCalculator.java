4a07f8493878878411b960806726e3fd16e60230
Vanessa Wingårdh <vwingardh@gmail.com>
2023-05-15 14:24:05 +0200

    Created regex pattern

--- src/main/java/calculator/StringCalculator.java
+++ src/main/java/calculator/StringCalculator.java
@@ -1,5 +1,8 @@
 package calculator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class StringCalculator {
     public int add(String[] integers) {
             if (integers.length == 0) {
@@ -10,7 +13,14 @@ public class StringCalculator {
                 if (nums == ""){
                     return 0;
                 }
+//                Pattern pattern = Pattern.compile(nums);
+//                System.out.println(pattern);
+//                Matcher matcher = pattern.matcher("([0-9]*,)");
+//                System.out.println(matcher);
             }
+
+            String[] splitString = integers.split(",");
+
             return 100;
         }
     }
